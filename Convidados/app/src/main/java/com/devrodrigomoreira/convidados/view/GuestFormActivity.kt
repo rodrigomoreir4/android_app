8f732f401a147c2d41bb1fba48061b54589ca29a
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-08-06 20:25:00 -0300

    Chamando inserção

--- Convidados/app/src/main/java/com/devrodrigomoreira/convidados/view/GuestFormActivity.kt
+++ Convidados/app/src/main/java/com/devrodrigomoreira/convidados/view/GuestFormActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.devrodrigomoreira.convidados.R
 import com.devrodrigomoreira.convidados.databinding.ActivityGuestFormBinding
+import com.devrodrigomoreira.convidados.model.GuestModel
 import com.devrodrigomoreira.convidados.viewModel.GuestFormViewModel
 
 class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
@@ -27,7 +28,11 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         if (v.id == R.id.button_save) {
+            val name = binding.editName.text.toString()
+            val presence = binding.radioPresent.isChecked
 
+            val model = GuestModel(0, name, presence)
+            viewModel.insert(model)
         }
     }
 }
--- Convidados/app/src/main/java/com/devrodrigomoreira/convidados/viewModel/GuestFormViewModel.kt
+++ Convidados/app/src/main/java/com/devrodrigomoreira/convidados/viewModel/GuestFormViewModel.kt
@@ -3,10 +3,15 @@ package com.devrodrigomoreira.convidados.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.devrodrigomoreira.convidados.model.GuestModel
 import com.devrodrigomoreira.convidados.repository.GuestRepository
 
 class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = GuestRepository.getInstance(application)
+
+    fun insert(guestModel: GuestModel){
+        repository.insert(guestModel)
+    }
 
 }
